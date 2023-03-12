f0ea1054e4d120b66d36a0c630836a63cb254cbb
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-12 09:46:32 +0600

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./containers/home";
+import Publish from "./containers/publish";
+import Layout from "./layout";
+import { ROUTES } from "./utils/constants";
 
 const App = () => {
-  return <div>branch development</div>;
+  return (
+    <>
+      <Routes>
+        <Route path={ROUTES.MAIN} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={ROUTES.PUBLISH} element={<Publish />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;

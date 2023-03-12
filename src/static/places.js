b172848965eb841bd814a0cae4b145e721401219
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-12 10:17:19 +0600

    change some position and add

--- src/static/places.js
+++ src/static/places.js
@@ -3,18 +3,24 @@ const places = [
   {
     id: 1,
     title: "Green",
-    description:
+    content:
       "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
-    imageUrl: image,
     time: 1500,
+    tags: [],
+    likes: 0,
+    rating: 0,
+    images: [image, image, image, image],
   },
   {
     id: 2,
-    title: "Bora Bora",
-    description:
+    title: "Red",
+    content:
       "Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.",
-    imageUrl: image,
     time: 1500,
+    tags: [],
+    likes: 0,
+    rating: 0,
+    images: [image, image, image, image],
   },
 ];
 
